edcc4dc7cf335c73cc6535cebed6d105e4110dda
Victor Alves Perdigão Assis <victorp@lnxcit012239bh.cit>
2018-03-14 11:28:37 -0300

    Include all the basic files

---
+++ js/controller.js
@@ -0,0 +1,14 @@
+function emociona() {
+    var sound = document.getElementById("emocionaAudio");
+    sound.play();
+}
+
+function naoToRindo() {
+    var sound = document.getElementById("naoToRindoAudio");
+    sound.play();
+}
+
+function zumba() {
+    var sound = document.getElementById("zumbaAudio");
+    sound.play();
+}
